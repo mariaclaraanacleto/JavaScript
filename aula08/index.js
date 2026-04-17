8afd0533f855f377953527f404e361e719c579e9
Maria Clara <223488871+mariaclaraanacleto@users.noreply.github.com>
2026-04-17 16:46:21 -0300

    dados primitivos

---
+++ aula08/index.js
@@ -0,0 +1,8 @@
+const nome = 'Maria'; //String
+const nome1 = 'Maria'; //String
+const nome2 = 'Maria'; //String
+const num1 = 5;
+const num2 = 10.52;
+let nomeAluno; // undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; //Nulo -> não aponta para local nenhum na memória
+const aprovado = false; // Boolean -> true, false (Lógico)
