8ce499e126ac70395eae7c8e9f8ac91f78b82ecb
Maria Clara <223488871+mariaclaraanacleto@users.noreply.github.com>
2026-04-22 14:45:05 -0300

    aritmeticos

---
+++ aula09/index.js
@@ -0,0 +1,16 @@
+/**
+ * * Aritomético em sequência
+ * () Potenciação
+ * **
+ * * / % Multiplicação / Divição / Resto da divição
+ * + Adição / Concatenação
+ * - Subtração
+ */
+ const num1 = 5;
+ const num2 = 2;
+ const num3 = 10;
+ console.log((num1 + num2) * num3);
+
+ let contador = 1;
+ contador++;
+ console.log(contador);
