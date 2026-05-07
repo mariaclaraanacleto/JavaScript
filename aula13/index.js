778c7111e37f16ca8b5eddbd1c0159400578bd8b
Maria Clara <223488871+mariaclaraanacleto@users.noreply.github.com>
2026-05-07 20:30:27 -0300

    aula13

---
+++ aula13/index.js
@@ -0,0 +1,23 @@
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2; //0.8
+num1 += num2; //0.9
+num1 += num2; //1.0
+num1 += num2; //1.1
+num1 += num2; //1.2
+num1 += num2; //1.3
+num1 += num2; //1.4
+num1 += num2; //1.5
+num1 += num2; //1.6
+num1 += num2; //1.7
+num1 += num2; //1.8
+num1 += num2; //1.9
+num1 += num2; //2.0
+
+mum1 = Number(num1.toFixed(2));
+console.log(num1);
+console.log(Number.isInteger(num1));
+
+
+
