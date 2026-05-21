68ef2a454edca86409180f476dbafd6f0110ec00
Maria Clara <223488871+mariaclaraanacleto@users.noreply.github.com>
2026-05-21 18:46:12 -0300

    aula16

---
+++ aula16/index.js
@@ -0,0 +1,30 @@
+const alunos =['Luiz', 'Maria', 'João']; //separar as coisas dentro com vírgula
+console.log(alunos);
+
+//console.log(typeof alunos); // retornará object
+//console.log(alunos instanceof Arraw); //retornará true ou false
+
+//console.log(alunos.slice(0, 2));
+
+//delete alunos[]; // deletou e aparecerá como elemento vazio
+
+//alunos.pop(); // remove o elemento do final
+//alunos.shift(); // remove o elemento do começo
+//const removidos = alunos.pop() ou alunos.shift(); // ve qual elemento foi removido
+
+//alunos.unshift('Luiza'); // Adiciona no começo
+//alunos.unshift('Fabio');
+
+//alunos.push('Luiza'); // Adiciona no fim
+//alunos.push('Fabio');
+
+//alunos[alunos.length] = 'Luiza'; // Adiona no fim
+//alunos[alunos.length] = 'Fabio';
+//alunos[alunos.length] = 'Carol';
+
+//alunos[0] = 'Eduardo'; // Altera
+//alunos[3] = 'Luiza'; // Adiciona
+
+//console.log(alunos);
+//console.log(alunos[2]);
+//o índice do Arraws conta por elemento
