1329d7f2f6d128c8aa9c7444ec9e67a55d66f32f
Maria Clara <223488871+mariaclaraanacleto@users.noreply.github.com>
2026-05-22 16:10:00 -0300

    aula17

---
+++ aula17/index.js
@@ -0,0 +1,19 @@
+//const raiz = function(n) {
+//    return n ** 0.5;
+//};
+//console.log(raiz(9));
+//console.log(raiz(16));
+//console.log(raiz(25));
+
+//function soma(x, y){
+//    const resultado = x + y;
+//    return resultado;
+//}
+//const resultado = soma(4, 2);
+//console.log(resultado);
+
+//function saudacao(nome){
+//    return `Bom dia ${nome}!`;
+//}
+//const variavel = saudacao('Maria');
+//console.log(variavel);
