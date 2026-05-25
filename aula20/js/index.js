146c4b5d520db8984923c4ecb8ac39812f523d92
Maria Clara <223488871+mariaclaraanacleto@users.noreply.github.com>
2026-05-25 19:16:29 -0300

    começo aula20

---
+++ aula20/js/index.js
@@ -0,0 +1,20 @@
+function meuEscopo () {
+    const form = document.querySelector('.form');
+    
+        //form.onsubmit =  function (evento) {
+        //    EventCounts.preventDefault();
+        //    alert(1);
+        //    console.log('Foi Enviado;');
+        //};
+
+        let contador = 1;
+        function recebeEventoForm (evento){
+            evento.preventDefault();
+            console.log(`Form não foi enviado ${contador}.`);
+            contador++
+        }
+
+        form.addEventListener('submit', recebeEventoForm);
+}
+ 
+meuEscopo ()
