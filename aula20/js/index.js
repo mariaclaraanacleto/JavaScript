2b87c9dac834a947804d3b8c85aa31d7d26375ac
Maria Clara <223488871+mariaclaraanacleto@users.noreply.github.com>
2026-05-29 14:45:32 -0300

    aula20

--- aula20/js/index.js
+++ aula20/js/index.js
@@ -1,17 +1,26 @@
 function meuEscopo () {
     const form = document.querySelector('.form');
-    
-        //form.onsubmit =  function (evento) {
-        //    EventCounts.preventDefault();
-        //    alert(1);
-        //    console.log('Foi Enviado;');
-        //};
+    const resultado = document.querySelector('.resultado');
+    const pessoas = []
 
-        let contador = 1;
         function recebeEventoForm (evento){
             evento.preventDefault();
-            console.log(`Form não foi enviado ${contador}.`);
-            contador++
+
+            const nome = form.querySelector('.nome');
+            const sobrenome = form.querySelector('.sobrenome');
+            const peso = form.querySelector('.peso');
+            const altura = form.querySelector('.altura');
+
+            pessoas.push({
+                nome: nome.value,
+                sobrenome: sobrenome.value,
+                peso: peso.value,
+                altura: altura.value
+            });
+
+            console.log(pessoas);
+
+            resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
         }
 
         form.addEventListener('submit', recebeEventoForm);
