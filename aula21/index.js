892cf3261658f61c4cf304caaf020fe150e8ed04
Maria Clara <223488871+mariaclaraanacleto@users.noreply.github.com>
2026-05-29 16:37:08 -0300

    aula21 operadores lógicos

---
+++ aula21/index.js
@@ -0,0 +1,20 @@
+/*
+Operadores de comparação
+> Maior que
+>= Maior que ou igual a
+< Menor que
+<= Menor que ou igual a
+=== Igualdade estrita (valor e tipo)
+!== Diferente estrito (valor e tipo)
+*/
+
+/*
+Operadores lógicos
+&& -> AND: Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR: Se tiver uma coisa verdadeira ele já retorna um true, para retornar false, todas tem que ser false
+! -> NOT: Ele inverte os valores
+*/
+
+//const expressaoAnd = true && true && true && true;
+// const expressaoOr = false || false || false || false;
+// console.log(!true); // inverteu para false
