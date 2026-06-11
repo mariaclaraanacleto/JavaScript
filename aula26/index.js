5d711172878ac34107d208855c9d5bfb1e8f65ae
Maria Clara <223488871+mariaclaraanacleto@users.noreply.github.com>
2026-06-11 15:34:06 -0300

    aula26 ? :

---
+++ aula26/index.js
@@ -0,0 +1,10 @@
+// ? = Verdadeiro : = Falso
+
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal';
+
+//if (pontuacaoUsuario >= 1000){
+//  console.log('Usuário VIP');
+//} else{
+//  console.log('Usuário Normal');
+//}
