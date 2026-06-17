bc4939f29902b4796642da5aa1e59659bb830d57
Maria Clara <223488871+mariaclaraanacleto@users.noreply.github.com>
2026-06-17 16:04:31 -0300

    aula29

---
+++ aula29/index.js
@@ -0,0 +1,34 @@
+const verdadeira = true;
+
+//Let tem escopo de bloco {...bloco}
+//Var só tem escopo de função
+
+//let nome = 'Luiz'; //criando
+// nome2 = 'Luiz'; //criando
+
+//if (verdadeira){
+//    let nome = 'Otavio'; //criando
+//    var nome2 = 'Rogerio'; //redeclarando
+//
+//    if (verdadeira){
+//        var nome2 = 'Ronaldo'; //redeclarando
+//        let nome = 'Outra coisa';
+//    }
+//}
+
+//console.log(nome, nome2);
+
+//function falaOi(){
+//    if (verdadeira){
+//        let nome = 'Luiz;'
+//        var sobrenome = 'Miranda';
+//    }
+//
+//    console.log(sobrenome);
+//
+//}
+//
+//falaOi()
+
+//console.log(sobrenome); // console.log antes do var, let, const, aparecerá como undefined
+//var sobrenome = 'Miranda';
